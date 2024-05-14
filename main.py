5abbb18765df1ab870839435cea21c256086d470
Артём <arrybin@icloud.com>
2024-05-14 20:57:08 +0300

    2 коментарий

--- main.py
+++ main.py
@@ -18,10 +18,3 @@ def simnum(num):
 func = simnum(num)
 print(func)
 
-echo "# 1st" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/RybinArtem/1st.git
-git push -u origin main
