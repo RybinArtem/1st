73096ac0161fe80fae3f231d128ba8ce4166b93a
Артём <arrybin@icloud.com>
2024-05-23 17:39:02 +0300

    без счетчика

--- main.py
+++ main.py
@@ -3,19 +3,14 @@ import random
 num = int(input())
 
 def simnum(num):
-    j = 0
     if num in [0,1]:
         return 'Особое число'
     else:
-        for i in range(2,num+1):
+        for i in range(2,num):
             if num%i == 0:
-                j += 1
-        if j == 1:
-            return 'Простое число'
-        else:
-            return 'Составное число'
+                return 'Составное число'
+        return 'Простое число'
 
 func = simnum(num)
 print(func)
 
-print()
